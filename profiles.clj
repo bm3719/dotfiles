fb8c8352e9e829aad94c3d5ca7089d8d24dbd13d
Bruce C. Miller <bm3719@gmail.com>
2016-08-03 12:08:40 -0400

    Update profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,7 @@
 {:user {:plugins [[lein-kibit "0.1.2"]
-                  [cider/cider-nrepl "0.8.2"]]
-        :dependencies [[spyscope "0.1.5"]]
+                  [lein-ancient "0.6.8"]
+                  [lein-try "0.4.3"]
+                  [cider/cider-nrepl "0.13.0"]]
+        :dependencies [[spyscope "0.1.5"]
+                       [org.clojure/tools.nrepl "0.2.12"]]
         :injections [(require 'spyscope.core)]}}

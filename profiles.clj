9657da465e9ef915509e8878f06b77651e314768
Bruce C. Miller <bm3719@gmail.com>
2017-01-15 01:43:30 -0500

    Update to CIDER 0.14
    This breaks spyscope.  Hopefully this is temporary.

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,8 @@
 {:user {:plugins [[lein-kibit "0.1.2"]
                   [lein-ancient "0.6.8"]
                   [lein-try "0.4.3"]
-                  [cider/cider-nrepl "0.13.0"]]
-        :dependencies [[spyscope "0.1.5"]
+                  [cider/cider-nrepl "0.14.0"]]
+        :dependencies [; [spyscope "0.1.5"]
                        [org.clojure/tools.nrepl "0.2.12"]]
-        :injections [(require 'spyscope.core)]}}
+        :injections [;(require 'spyscope.core)
+                     ]}}

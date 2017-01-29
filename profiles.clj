173d1b4cfd1d8094dff434826e347e41a01cba87
Bruce C. Miller <bm3719@gmail.com>
2017-01-29 03:45:33 -0500

    Add eastwood config

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,10 @@
 {:user {:plugins [[lein-kibit "0.1.2"]
                   [lein-ancient "0.6.8"]
                   [lein-try "0.4.3"]
+                  [jonase/eastwood "0.2.3"]
                   [cider/cider-nrepl "0.14.0"]]
         :dependencies [; [spyscope "0.1.5"]
                        [org.clojure/tools.nrepl "0.2.12"]]
         :injections [;(require 'spyscope.core)
-                     ]}}
+                     ]
+        :eastwood {:add-linters [:unused-locals :unused-namespaces]}}}

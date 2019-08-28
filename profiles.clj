4883e87c03de56870e74b39b5865c01dd514ef53
Bruce C. Miller <bm3719@gmail.com>
2019-08-27 23:13:48 -0400

    Bump plugin versions

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,11 @@
-{:user {:plugins [[lein-kibit "0.1.6"]
+{:user {:plugins [[lein-kibit "0.1.7"]
                   [lein-ancient "0.6.15"]
                   [lein-try "0.4.3"]
-                  [lein-bikeshed "0.5.1"]
-                  [jonase/eastwood "0.2.9"]
-                  [cider/cider-nrepl "0.17.0"]]
-        :dependencies [; [spyscope "0.1.5"]
+                  [lein-bikeshed "0.5.2"]
+                  [jonase/eastwood "0.3.6"]
+                  [cider/cider-nrepl "0.21.1"]]
+        :dependencies [;; [spyscope "0.1.6"]
                        [org.clojure/tools.nrepl "0.2.12"]]
-        :injections [;(require 'spyscope.core)
+        :injections [;; (require 'spyscope.core)
                      ]
         :eastwood {:add-linters [:unused-locals :unused-namespaces]}}}

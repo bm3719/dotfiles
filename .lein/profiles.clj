e5f9aab4bb90c6a9e23bbe22cac2be52266e3202
Bruce C. Miller <bm3719@gmail.com>
2021-03-28 10:59:07 -0400

    Bump cider-nrepl and tools.nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
 {:user {:plugins [[lein-ancient "0.6.15"]
                   [lein-try "0.4.3"]
                   [lein-bikeshed "0.5.2"]
-                  [cider/cider-nrepl "0.25.1"]]
+                  [cider/cider-nrepl "0.25.9"]]
         :dependencies [;; [spyscope "0.1.6"]
-                       [org.clojure/tools.nrepl "0.2.12"]]
+                       [org.clojure/tools.nrepl "0.2.13"]]
         :injections [;; (require 'spyscope.core)
                      ]
         :eastwood {:add-linters [:unused-locals :unused-namespaces]}}}

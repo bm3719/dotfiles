1a6fa7084dbde5580ad1cb7bf3190846753084c1
Bruce C. Miller <bm3719@gmail.com>
2021-04-12 13:57:12 -0400

    Bump lein plugin versions

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
-{:user {:plugins [[lein-ancient "0.6.15"]
+{:user {:plugins [[lein-ancient "1.0.0-RC3"]
                   [lein-try "0.4.3"]
                   [lein-bikeshed "0.5.2"]
-                  [cider/cider-nrepl "0.25.9"]]
+                  [cider/cider-nrepl "0.25.11"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]
         :injections []
         :eastwood {:add-linters [:unused-locals :unused-namespaces]}}}

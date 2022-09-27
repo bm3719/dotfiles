cd0c5be4f3f6f173b6c7f7f6c79e337685d92f03
Bruce C. Miller <bm3719@gmail.com>
2022-09-27 08:24:27 -0400

    Add lein-pprint to profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,8 @@
 {:user {:plugins [[lein-ancient "1.0.0-RC4-SNAPSHOT"]
                   [lein-try "0.4.3"]
                   [lein-bikeshed "0.5.2"]
-                  [cider/cider-nrepl "0.28.5"]]
+                  [cider/cider-nrepl "0.28.6"]
+                  [lein-pprint "1.3.2"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]
         :injections []
         :eastwood {:add-linters [:unused-locals :unused-namespaces]}}}

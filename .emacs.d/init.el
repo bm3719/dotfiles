580a559b333130bc57590e25c1a790151ff9d962
Bruce C. Miller <bm3719@gmail.com>
2022-09-27 08:24:38 -0400

    Add diminish mode hook for Clojure
    For unknown reasons, this doesn't work with the :diminish key in the
    use-package macro.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,7 @@
 ;;;; -*- mode: Emacs-Lisp; eldoc-mode:t -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Bruce C. Miller - bm3719@gmail.com
-;;;; Time-stamp: <2022-09-24 22:31:16 (bm3719)>
+;;;; Time-stamp: <2022-09-27 08:23:26 (bm3719)>
 ;;;;
 ;;;; This init was created for GNU Emacs 27.1 for GNU/Linux, OpenBSD, and
 ;;;; Windows, but all or parts of this file should work with older GNU Emacs
@@ -532,7 +532,9 @@
   :ensure t
   :init
   (add-hook 'emacs-lisp-mode-hook
-            (lambda () (setq mode-name "e-λ"))))
+            (lambda () (setq mode-name "e-λ")))
+  (add-hook 'clojure-mode-hook
+            (lambda () (setq mode-name "cλj"))))
 
 (use-package counsel
   :ensure t

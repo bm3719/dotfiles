37f4f455abcb2edf3b0fd8c2e810e2628b35e165
Bruce C. Miller <bm3719@gmail.com>
2022-12-12 09:39:26 -0500

    Bump cider-nrepl version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[lein-ancient "1.0.0-RC4-SNAPSHOT"]
                   [lein-try "0.4.3"]
                   [lein-bikeshed "0.5.2"]
-                  [cider/cider-nrepl "0.28.6"]
+                  [cider/cider-nrepl "0.29.0"]
                   [lein-pprint "1.3.2"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]
         :injections []

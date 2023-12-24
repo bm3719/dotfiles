765bbe7d958895fd42e825dc2192ebc80ef911bd
Bruce C. Miller <bm3719@gmail.com>
2023-12-24 12:53:10 -0500

    Add binding for counsel-fzf

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,7 @@
 ;;;; -*- mode: Emacs-Lisp; eldoc-mode:t -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Bruce C. Miller - bm3719@gmail.com
-;;;; Time-stamp: <2023-12-22 23:12:03 (bm3719)>
+;;;; Time-stamp: <2023-12-23 12:20:32 (bm3719)>
 ;;;;
 ;;;; This init was created for GNU Emacs 28.2 for GNU/Linux, OpenBSD, and
 ;;;; Windows, but all or parts of this file should work with older GNU Emacs
@@ -587,6 +587,7 @@ If the file doesn't exist, return an empty string."
   ("C-c v"   . ivy-push-view)
   ("C-c V"   . ivy-pop-view)
   ("C-x l"   . counsel-locate)
+  ("C-x C-l" . counsel-fzf)
   ("C-x g"   . counsel-rg))
 
 (use-package ivy-prescient

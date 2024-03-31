cac83c40b6a3c78a7e841f2e05982b5bc9b1141d
Bruce C. Miller <bm3719@gmail.com>
2024-03-31 11:59:47 -0400

    Remove Time-stamp tracking
    This can be a handy feature when jumping through history, but gets in
    the way when merging multiple branches.  Will re-add if I go back to
    having a single version.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,6 @@
 ;;;; -*- mode: Emacs-Lisp; eldoc-mode:t -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Bruce C. Miller - bm3719@gmail.com
-;;;; Time-stamp: <2024-03-26 22:29:41 (bm3719)>
 ;;;;
 ;;;; This init was created for GNU Emacs 28.2 for GNU/Linux, OpenBSD, and
 ;;;; Windows, but all or parts of this file should work with older GNU Emacs
